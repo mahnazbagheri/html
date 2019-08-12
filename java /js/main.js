fea59823f64e7aab4351520f12cfc6defc278b9d
mahnazbagheri <52971114+mahnazbagheri@users.noreply.github.com>
2019-08-12 12:30:53 -0700

    java

---
+++ java /js/main.js
@@ -0,0 +1,17 @@
+
+
+var date=new Date();
+
+
+document.getElementById('fromjs').innerHTML=date;
+
+
+
+
+
+alert("now I have js")
+
+
+
+
+document.getElementsByClassName('myclass')[2].innerHTML="these are classes";
